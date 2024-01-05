d3227d3356db75307695adfaf76caae9c0e11a71
roker16 <154746611+roker16@users.noreply.github.com>
2024-01-05 16:47:32 +0530

    milestone-image-upload finished

--- zixnote/app/api/uploadImage/route.ts
+++ zixnote/app/api/uploadImage/route.ts
@@ -1,60 +1,39 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import * as formidable from "formidable";
-import { Files } from "formidable";
-import { v4 as uuid } from "uuid";
-// import error from "next/error";
-// import { supabase } from "../../lib/supabaseClient";
-// import { useUser } from "@supabase/auth-helpers-react";
-import fs from "fs";
-
-import { cookies } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
+import { NextApiResponse } from "next";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-
-// import { supabaseServerClient } from "../../lib/supabaseClient";
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+import { v4 as uuid } from "uuid";
 
 export async function POST(
   req: Request,
   res: NextApiResponse
   //   file: string
 ) {
-  // const supabase = await supabaseServerClient({ req })
-
-  // let error1 = null;
-  // let publicurl = null;
-  // let name = null;
-
-  // const { user } = await supabaseClient.auth.api.getUserByCookie(req);
-  // if (user == null) {
-  //   console.log("not user");
-  // } else {
-  //   console.log("user hai" + user);
-  // }
   const formData = await req.formData();
-  const file = formData.get("file-0");
-  if (file instanceof File) {
-    console.log("madarchod");
-  } else {
-    console.log("bahinchod");
+  // Convert the iterable to an array
+
+  const { file, fileName } = getFileDetail(formData);
+  if (!file || !fileName) {
+    return NextResponse.json(
+      { message: "File not found" },
+      {
+        status: 405,
+      }
+    );
   }
   const supabase = createClient(cookies());
+
+  const fileNameTobeUploaded = generateUniqueFileName(fileName);
   const { data, error } = await supabase.storage
     .from("suneditor")
-    .upload("public/avatar1", file!, {
+    .upload(`public/${fileNameTobeUploaded}`, file, {
       cacheControl: "3600",
       upsert: false,
     });
   if (data) {
     const { data } = await supabase.storage
       .from("suneditor")
-      .getPublicUrl("public/avatar1");
-    console.log("public url is ", data.publicUrl);
-
+      .getPublicUrl(`public/${fileNameTobeUploaded}`);
     return NextResponse.json(
       {
         errorMessage: error,
@@ -71,12 +50,32 @@ export async function POST(
       }
     );
   }
-  console.log("form data", typeof file);
-
-  console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
-  const form = new formidable.IncomingForm({ keepExtensions: true });
-  console.log("kkkkkkkkkkkkkkkkkkkk", JSON.stringify(form));
-
-  
   return res.status(405).json({ message: "Method not allowed." });
+}
+
+function getFileDetail(formData: FormData) {
+  const entriesArray = Array.from(formData.entries());
+  // Iterate over the array
+  let fileName, file;
+  for (const [name, value] of entriesArray) {
+    // Check if the value is a File object
+    if (value instanceof File) {
+      file = value;
+      fileName = value.name;
+      // You can break the loop if you only need the first file
+      break;
+    }
+  }
+  return { file, fileName };
+}
+
+function generateUniqueFileName(originalFileName: string) {
+  const newUuid = uuid();
+  const fileNameWithoutExtension =
+    originalFileName.slice(0, originalFileName.lastIndexOf(".")) ||
+    originalFileName;
+  const lowercaseFileNameWithoutSpace = `${fileNameWithoutExtension
+    .replace(/\s+/g, "-")
+    .toLowerCase()}-${newUuid}`;
+  return lowercaseFileNameWithoutSpace;
 }
