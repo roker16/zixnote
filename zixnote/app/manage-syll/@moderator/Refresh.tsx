ae459784aa453f420919cb97b916ad3763ac2f70
roker16 <154746611+roker16@users.noreply.github.com>
2024-04-14 10:31:14 +0530

    as

--- zixnote/app/manage-syll/@moderator/Refresh.tsx
+++ zixnote/app/manage-syll/@moderator/Refresh.tsx
@@ -1,21 +1,23 @@
 "use client";
 import { usePathname, useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Refresh() {
   const router = useRouter();
   const path = usePathname();
-//   useEffect(() => {
-//     router.refresh();
-//   }, [path,router]);
+  const [counter, setCounter] = useState(0);
+  //   useEffect(() => {
+  //     router.refresh();
+  //   }, [path,router]);
 
   return (
     <div>
-      {" "}
+      {counter}
       <button type="button" onClick={() => router.refresh()}>
         Refresh
       </button>
-      {path} 
+      <button onClick={() => setCounter(counter + 1)}>increase</button>
+      {path}
     </div>
   );
 }
