368e1693d645f50d0e88a4a3a88befd24d164ef8
roker16 <154746611+roker16@users.noreply.github.com>
2024-04-17 02:23:07 +0530

    as

--- zixnote/app/action.ts
+++ zixnote/app/action.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import { wait } from "@/utils/helper";
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
