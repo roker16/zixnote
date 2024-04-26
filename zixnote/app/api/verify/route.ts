0174d9eb7d22747ffd16032c2b479cca4dc0e94d
roker16 <154746611+roker16@users.noreply.github.com>
2024-04-26 10:54:42 +0530

    as

--- zixnote/app/api/verify/route.ts
+++ zixnote/app/api/verify/route.ts
@@ -1,26 +1,24 @@
 import { Cashfree } from "cashfree-pg";
 
 export async function POST(request: Request) {
-  Cashfree.XClientId = process.env.NEXT_PUBLIC_CLIENT_ID;
-  Cashfree.XClientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
-  Cashfree.XEnvironment = Cashfree.Environment.PRODUCTION;
   try {
-    const { orderId } = await request.json();
+    // Set Cashfree credentials
+    Cashfree.XClientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+    Cashfree.XClientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
+    Cashfree.XEnvironment = Cashfree.Environment.PRODUCTION;
 
-    Cashfree.PGOrderFetchPayments("2023-08-01", orderId)
-      .then((response) => {
-        return new Response(JSON.stringify(response.data), { status: 200 });
-      })
-      .catch((error) => {
-        console.error(error.response.data.message);
-        console.error("verify error is ", JSON.stringify(error));
-        return new Response("An error occurred while processing your request.", {
-          status: 500,
-        });
-      });
+    // Parse request body
+    const body = await request.json();
+    const { orderId } = body;
+
+    // Fetch payments
+    const response = await Cashfree.PGOrderFetchPayments("2023-08-01", orderId);
+
+    // Return response
+    return new Response(JSON.stringify(response.data), { status: 200 });
   } catch (error) {
-    console.error("verify error is ", JSON.stringify(error));
-    console.log(error);
+    // Handle errors
+    console.error("An error occurred:", error);
     return new Response("An error occurred while processing your request.", {
       status: 500,
     });
