1f9ba90b09d5b183087595b5d7b843fc77adfdf2
roker16 <official15.rajiv@gmail.com>
2024-10-19 11:53:01 +0530

    as

--- zixnote/app/page.tsx
+++ zixnote/app/page.tsx
@@ -17,6 +17,7 @@ export default async function Index() {
     <div>
       <HeaderMegaMenu user={user} />
       <Container size={"xl"} >
+        hello
         <HeroBullets />
       </Container>
       
